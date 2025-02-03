b53fe309e15963345a85f6bdcd24e4f5d158123e
lcymicwork <143369553+lcymicwork@users.noreply.github.com>
2025-02-03 18:03:34 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My Blank Website</h1>
+      <p>This is a simple blank website for presentation use.</p>
+      <p><a href="template.html">View Template</a></p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./template.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
